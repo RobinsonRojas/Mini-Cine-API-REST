dea1f9222be48bc09f636d6b226d0a3f4dc62b94
Robinson Arley Rojas Novoa <totorox123@gmail.com>
2021-04-03 00:21:55 -0500

    added method delete and syntax

--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -2,65 +2,79 @@ const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 
-//Obteniendo el conector de la base de datos
+//Obteniendo el conector de la DB
 const mysqlConnection = require('../database');
 
-//Obteniendo datos globales de la tabla en la base de datos
+//Obteniendo datos globales de la tabla en la DB
 router.get('/', (req, res) => {
-    console.log("XXX LINE 9");
+    console.log("Metodo get read all");
     mysqlConnection.query('SELECT * FROM usuario', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
         } else {
-            console.log(err , " Error en la consulta");
+            console.log(err, " Error en la consulta");
         }
     });
 });
 
-//Obteniendo datos especificos de la tabla en la base de datos
+//Obteniendo datos especificos de la tabla en la DB
 router.get('/:id', (req, res) => {
     const { id } = req.params;// { id } :: '/:id' 
-    console.log(id, "LINE 21");
-    mysqlConnection.query('SELECT * FROM usuario where id_usuario = ?', [id], (err, rows, fields)  =>{
+    console.log(id, "Metodo get specific read");
+    mysqlConnection.query('SELECT * FROM usuario where id_usuario = ?', [id], (err, rows, fields) => {
         if (!err) {
             res.json(rows[0]);
-        } else {    
-            console.log(err , " Error en la consulta");
+        } else {
+            console.log(err, " Error en la consulta");
         }
-    } );
-});//router.get
+    });
+});
 
 //Insertar usuarios a la tabla de la DB
-router.post('/', (req, res) =>{
-    const {id, name} = req.body;//Opteniendo los datos desde Cliente
-    console.log(id," ",name, "LINE 34");
+router.post('/', (req, res) => {
+    //Opteniendo los datos desde Cliente
+    const { id, name } = req.body;
+    console.log(id, " ", name, "Metodo post Insert");
     const query = `
         SET @id = ?;
         SET @name = ?;
         CALL usuarioAddOrEdit(@id, @name);
     `;
-    mysqlConnection.query(query, [id, name], (err, rows, fields) =>{
-        if(!err){
-            res.json({Status:'Usuario guardado'})
-        }else{
+    mysqlConnection.query(query, [id, name], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Usuario guardado' })
+        } else {
             console.log(err)
         }
     });
 });
 
 //Actializar datos de la tabla en la DB
-router.put('/:id', (req, res) =>{
-    const {name} = req.body;
+router.put('/:id', (req, res) => {
+    const { name } = req.body;
     const { id } = req.params;
-    console.log(id," ",name, "LINE 52 XXXXX");
+    console.log(id, " ", name, "Metodo put update");
     const query = 'CALL usuarioAddOrEdit(?, ?)';
-    mysqlConnection.query(query, [id, name], (err, rows, fields) =>{
-        if(!err){
-            res.json({Status:'Usuario Actualizado'})
-        }else{
-            console.log(err)
+    mysqlConnection.query(query, [id, name], (err, rows, fields) => {
+        if (!err) {
+            res.json({ Status: 'Usuario Actualizado' })
+        } else {
+            console.log(err);
         }
     });
 });
+
+//Elimindao datos de la tabla en la DB
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id, "Metodo delete");
+    mysqlConnection.query('DELETE FROM usuario  WHERE id_usuario = ?', [id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: 'Usuario eliminado' });
+        } else {
+            console.log(err);
+        }
+    })
+})
 
 module.exports = router;

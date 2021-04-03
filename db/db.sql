e12da7bbdec08f74a2ac493ee8279556eed4789e
John Villamil <johnsteve.v997@gmail.com>
2021-04-02 22:02:12 -0500

    Añadidos los metodos de insertar y actualizar usuario en la DB

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,6 @@
+-- para el Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client.
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+
 CREATE DATABASE IF NOT EXISTS cinema;
 use cinema;
 CREATE TABLE IF NOT EXISTS usuario(
@@ -6,7 +9,7 @@ CREATE TABLE IF NOT EXISTS usuario(
     PRIMARY KEY(id_usuario)
 );
 
-
+SELECT * FROM usuario;
 insert into usuario (nombre_usuario) values ('Aldwin');
 insert into usuario (nombre_usuario) values ('Joli');
 insert into usuario (nombre_usuario) values ('Faydra');
@@ -38,5 +41,26 @@ insert into usuario (nombre_usuario) values ('Basia');
 insert into usuario (nombre_usuario) values ('Hilario');
 insert into usuario (nombre_usuario) values ('Eb');
 
--- para el Error: ER_NOT_SUPPORTED_AUTH_MODE: Client does not support authentication protocol requested by server; consider upgrading MySQL client.
--- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345';
+
+-- PROCEDIMIENTO ALAMACENADO
+CREATE PROCEDURE usuarioAddOrEdit (
+    IN _id INT,
+    IN _nombre VARCHAR(45)
+)
+
+BEGIN 
+    IF _id = 0 THEN
+        INSERT INTO usuario(nombre_usuario) 
+        VALuES(_nombre);
+        SET _id = LAST_INSERT_ID();
+    ELSE
+        UPDATE usuario
+        SET
+            nombre_usuario = _nombre
+        WHERE id_usuario = _id;
+    END IF;
+    SELECT _id AS id_usuario;
+END
+;
+
+drop PROCEDURE usuarioAddOrEdit;
